ee2de4d694633d671e4d3de714352f206bdaa415
IPIA-14 <ipiajuanjo7@gmail.com>
2026-08-25 13:54:45 -0500

    Despliegue hibrido

---
+++ src/environments/environment.ngrok.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  name: "ngrok",
+  origin: "HyL-Sparta-V2",
+  production: false,
+
+  auth: '/auth/login',
+  services: '/api',
+  menu: '/api/menu/ejecutarMenus'
+};
