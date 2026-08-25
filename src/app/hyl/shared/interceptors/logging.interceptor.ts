bbb17ae857df8a5e60f63f3f4854b88569ffa2a7
IPIA-14 <ipiajuanjo7@gmail.com>
2026-08-25 14:01:54 -0500

    fix: bypass ngrok browser warning

--- src/app/hyl/shared/interceptors/logging.interceptor.ts
+++ src/app/hyl/shared/interceptors/logging.interceptor.ts
@@ -3,5 +3,13 @@ import { Observable } from 'rxjs';
 
 export function LoggingInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
   console.log(`[API Request] Ruta consumida: ${req.method} ${req.urlWithParams}`);
-  return next(req);
+  
+  // Clonar la petición para agregar el header requerido por Ngrok Free
+  const modifiedReq = req.clone({
+    setHeaders: {
+      'ngrok-skip-browser-warning': 'true'
+    }
+  });
+
+  return next(modifiedReq);
 }
